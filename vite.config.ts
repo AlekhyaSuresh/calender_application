df66c0797e14eafa2066abb1c9f8542c53ff7f0b
Alekhya Suresh Oruganti <138512768+AlekhyaSuresh@users.noreply.github.com>
2025-01-02 21:46:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/calender_application/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
